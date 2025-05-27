18d526aef62dea656b42cc23f9e3326749034fc0
jessekorff <jessekorff@gmail.com>
2025-05-27 11:57:52 -0400

    av updates

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,32 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
     {
-        img: "https://pixabay.com/images/download/business-20031_640.jpg",
+        img: "/conference_event_planner/projector.png",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/speakers-4109274_640.jpg",
+        img: "/conference_event_planner/speakers.png",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/public-speaking-3926344_640.jpg",
+        img: "/conference_event_planner/microphone.png",
         name: "Microphones",
         cost: 45,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/images/download/whiteboard-2903269_640.png",
+        img: "/conference_event_planner/whiteboard.png",
         name: "Whiteboards",
         cost: 80,
         quantity: 0,
     },
 
     {
-        img: "https://pixabay.com/images/download/signpost-235079_640.jpg",
+        img: "/conference_event_planner/signs.png",
         name: "Signage",
         cost: 80,
         quantity: 0,
@@ -40,10 +40,16 @@ export const avSlice = createSlice({
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action,payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }
     },
   },
 });
--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+import avReducer from './venueSlice';
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
   },
 });
