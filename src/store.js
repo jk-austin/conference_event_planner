f7267c330ade335156673b33114c61a39251b229
jessekorff <jessekorff@gmail.com>
2025-05-27 12:36:35 -0400

    mealsSlice update

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './venueSlice';
+import mealsReducer from './mealsSlice';
+
 export default configureStore({
   reducer: {
     venue: venueReducer,
